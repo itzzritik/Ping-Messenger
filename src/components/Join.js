805ac14db9790960471f1318d34b8772d157c659
Ritik Srivastava <ritik.space@gmail.com>
2019-10-07 01:25:23 +0530

    Developing Simple UI

--- src/components/Join.js
+++ src/components/Join.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
 
 const Join = () => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
     return(
-        <h1>Join</h1>
+        <div className="joinOuterContainer">
+            <div className='joinInnerContainer'>
+                <h1 className='heading'></h1>
+                <div><input placeholder="Name" className='joinInput' type='text' onChange='' /></div>
+                <div><input placeholder="Room" className='joinInput' type='text' onChange='' /></div>
+                <Link>
+                    <button className="button" type='submit'>SignIN</button>
+                </Link>
+            </div>
+        </div>
     )
 }
 
