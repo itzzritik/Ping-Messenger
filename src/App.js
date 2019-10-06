5cb20f66c12b5ff97c8b567e8610c92f073dd1c8
Ritik Srivastava <ritik.space@gmail.com>
2019-10-07 00:29:35 +0530

    Basic Start

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
-ReactDOM.render(<App />,document.querySelector('#root'));    
+import Join from './components/Join'
+import Chat from './components/Chat'
+
+const App = () => (
+    <Router>
+        <Route path="/" exact component={Join}/>
+        <Route path="/chat" component={Chat}/>
+    </Router>
+);
+
+export default App;
