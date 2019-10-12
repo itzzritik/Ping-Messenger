155a1848ce04f4cfb54618348c5b06c069636b19
Ritik Srivastava <ritik.space@gmail.com>
2019-10-12 23:48:20 +0530

    Redirecting Routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
+import Redirect from './components/Redirect/Redirect'
 import Join from './components/Join/Join'
 import Chat from './components/Chat/Chat'
 
 const App = () => (
     <Router>
-        <Route path="/" exact component={Join}/>
         <Route path="/chat" component={Chat}/>
+        <Route path="/login" exact component={Join}/>
+        <Route path="/*" exact component={Redirect}/>
     </Router>
 );
 
