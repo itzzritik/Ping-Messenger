1ec170934cbb27e38dd21bc5ea23547128f028cb
Ritik Srivastava <ritik.space@gmail.com>
2019-10-13 17:15:01 +0530

    Designing UI/UX

---
+++ src/components/Login/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Login.css';
+
+const Login = () => {
+    return(
+        <div className='mainPanel'>
+            <div className='titleDiv'>
+                <h1 className="title">PING MESSENGER</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
